3e19ab22150d4a0fa8604d42d883d98a99597270
LidiiaChyrnyshuk <l.mandych@gmail.com>
2025-05-30 16:36:38 +0200

    add new styles

--- js/slider.js
+++ js/slider.js
@@ -23,13 +23,22 @@ const swiper1 = new Swiper(".swiper-1", {
 	},
 });
 
+const swiper2 = new Swiper(".swiper-2", {
+	speed: 1000,
+	loop: true,
+	spaceBetween: 8,
+	slidesPerView: 1.3,
+	centeredSlides: false,
+	grabCursor: true, 
+});
+
 const swiper3 = new Swiper(".swiper-3", {
 	speed: 1000,
 	loop: true,
 	spaceBetween: 0,
 	slidesPerView: 1.5,
 	centeredSlides: false,
-	grabCursor: true, // показує "лапку" при наведенні
+	grabCursor: true, 
 });
 
 const swiper4 = new Swiper(".swiper-4", {
