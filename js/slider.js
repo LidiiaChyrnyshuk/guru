d7e9855df0397c41f06bf57d7b1fe00721ddcd82
LidiiaChyrnyshuk <l.mandych@gmail.com>
2025-06-05 15:43:18 +0200

    updete provider slider

--- js/slider.js
+++ js/slider.js
@@ -1,27 +1,4 @@
-/* const swiper1 = new Swiper(".swiper-1", {
-	speed: 1000,
-	loop: true,
-	spaceBetween: 10,
-	freeMode: true,
-	slidesPerView: 4.5,
-	freeModeMomentum: false,
-	allowTouchMove: false,
-	simulateTouch: false,
-	autoplay: {
-		delay: 0,
-		disableOnInteraction: false,
-	},
-	breakpoints: {
-		768: {
-			slidesPerView: 8.5,
-			spaceBetween: 30,
-		},
-		1024: {
-			slidesPerView: 10.5,
-			spaceBetween: 20,
-		},
-	},
-}); */
+
 
 const swiper2 = new Swiper(".swiper-2", {
 	speed: 1000,
@@ -41,25 +18,4 @@ const swiper3 = new Swiper(".swiper-3", {
 	grabCursor: true, 
 });
 
-const swiper4 = new Swiper(".swiper-4", {
-	speed: 2000,
-	loop: true,
-	spaceBetween: 20,
-	slidesPerView: 3,
-	allowTouchMove: false,
-	simulateTouch: false,
-	autoplay: {
-		delay: 0,
-		disableOnInteraction: false,
-	},
-	breakpoints: {
-		768: {
-			slidesPerView: 5,
-			spaceBetween: 30,
-		},
-		1024: {
-			slidesPerView: 5,
-			spaceBetween: 40,
-		},
-	},
-});
+
