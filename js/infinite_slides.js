c22555b421f90eb381dc5a634cbadca8a3440228
LidiiaChyrnyshuk <l.mandych@gmail.com>
2025-06-05 17:14:32 +0200

    change ellips500

--- js/infinite_slides.js
+++ js/infinite_slides.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const track = document.getElementById("infiniteTrack");
 	const slider = document.querySelector(".infinite-slider");
-	const speed = 0.8; // пікселів за кадр
+	const speed = 0.8; 
 	let offset = 0;
 
-	// 1. Клонуємо вміст 3 рази для безперервності
+	
 	const originalSlides = Array.from(track.children);
 	for (let i = 0; i < 3; i++) {
 		originalSlides.forEach((slide) => {
@@ -13,13 +13,13 @@ document.addEventListener("DOMContentLoaded", () => {
 		});
 	}
 
-	// 2. Анімація
+	
 	function animate() {
 		offset += speed;
 		track.style.transform = `translateX(-${offset}px)`;
 
 		if (offset >= track.scrollWidth / 3) {
-			// Скидаємо, не видно бо дублікати є
+	
 			offset = 0;
 			track.style.transform = `translateX(0)`;
 		}
